52fcc2ea143e972c863b0531a87f0b22878f1c87
Alan Lima <dev.alanlima@gmail.com>
2023-06-25 10:25:35 -0300

    add / colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html","./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        amarelo: '#FBC508',
+        branco: '#FFFFFF',
+        cinza_escuro: '#363636',
+        roxo_escuro: '#2B0955',
+        roxo_claro: '#AE97CB',
+        roxo: '#7000FF',
+      },
+      fontFamily:{
+        'poppins': ['Poppins', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
