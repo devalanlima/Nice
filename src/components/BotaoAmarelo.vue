16d6f92735037e4c0136beaa5d850954f0a1936b
Alan Lima <dev.alanlima@gmail.com>
2023-06-25 13:34:33 -0300

    create / BotaoAmarelo component

---
+++ src/components/BotaoAmarelo.vue
@@ -0,0 +1,11 @@
+<template>
+    <button class="bg-amarelo px-12 py-3 self-baseline rounded-lg font-semibold text-roxo_escuro">
+        {{props.text}}
+    </button>
+</template>
+
+<script setup>
+const props = defineProps({
+    text:{Type:String, default:'Botão'}
+})
+</script>
