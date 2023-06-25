1816b4b1b74f0d959028513c7a0a005c3c227b00
Alan Lima <dev.alanlima@gmail.com>
2023-06-25 13:35:01 -0300

    create / BotaoTransparente component

---
+++ src/components/BotaoTransparente.vue
@@ -0,0 +1,11 @@
+<template>
+    <button class="px-10 py-3 border-[1px] border-branco self-baseline rounded-lg font-semibold text-branco">
+        {{ props.text }}
+    </button>
+</template>
+
+<script setup>
+const props = defineProps({
+    text:{Type:String, default:'Botão'}
+})
+</script>
