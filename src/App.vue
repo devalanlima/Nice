919ba592dd61dfc3909077d46e56533b7799ca72
Alan Lima <dev.alanlima@gmail.com>
2023-06-25 13:36:17 -0300

    create / main

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup>
 import NavBar from './components/NavBar.vue';
+import BannerPrincipal from './components/BannerPrincipal.vue'
 </script>
 
 <template>
   <header class="mb-6 px-5 pt-10">
     <NavBar/>
   </header>
+  <main>
+    <BannerPrincipal/>
+  </main>
 </template>
