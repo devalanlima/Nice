20fcab17500a326153fd4a79bc680850a3434c58
Alan Lima <dev.alanlima@gmail.com>
2023-06-26 13:08:23 -0300

    fix / changed lenght percentage and bg-hover percentage to resolve visual bug

--- src/components/BotaoAmarelo.vue
+++ src/components/BotaoAmarelo.vue
@@ -1,5 +1,5 @@
 <template>
-    <button class="bg-[0%] bg-[length:200%_100%] bg-gradient-to-r from-amarelo from-50% to-transparent to-0% hover:transition-all transition-all hover:text-amarelo box-border px-12 py-3 self-baseline rounded-lg font-semibold text-roxo_escuro hover:bg-[100%] border-2 border-amarelo ">
+    <button class="bg-[0%] bg-[length:205%_100%] bg-gradient-to-r from-amarelo from-50% to-transparent to-0% hover:transition-all transition-all hover:text-amarelo box-border px-12 py-3 self-baseline rounded-lg font-semibold text-roxo_escuro hover:bg-[99%] border-[1px] border-amarelo ">
         {{props.text}}
     </button>
 </template>
--- src/components/BotaoTransparente.vue
+++ src/components/BotaoTransparente.vue
@@ -1,6 +1,6 @@
 <template>
     <button class="px-10 py-3 border-[1px] border-branco self-baseline rounded-lg font-semibold text-branco
-    bg-gradient-to-r from-transparent from-50% to-branco to-50% bg-[length:200%_100%] hover:bg-[100%] transition-all hover:transition-all hover:text-roxo_escuro
+    bg-gradient-to-r from-transparent from-50% to-branco to-50% bg-[length:205%_100%] hover:bg-[99%] transition-all hover:transition-all hover:text-roxo_escuro z-10 hover:border-transparent
     ">
         {{ props.text }}
     </button>
