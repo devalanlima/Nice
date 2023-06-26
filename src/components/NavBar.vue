0d4f8ceaa6e1e5ede5d6cf4766d2d1c7b86766a5
Alan Lima <dev.alanlima@gmail.com>
2023-06-26 12:23:51 -0300

    stylize / change button menu thickness

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,11 +4,11 @@
             <div
                 class="transition-all before:transition-all after:transition-all relative group-hover:w-[20px] group-hover:before:w-[30px] group-hover:after:w-[20px]
 
-                w-[30px] h-[6px] bg-black rounded-sm
+                w-[30px] h-[5px] bg-black rounded-sm
 
-                before:w-[20px] before:h-[6px] before:bg-black before:rounded-sm before:block before:absolute before:top-[11px] before:left-0
+                before:w-[20px] before:h-[5px] before:bg-black before:rounded-sm before:block before:absolute before:top-[11px] before:left-0
 
-                after:w-[30px] after:h-[6px] after:bg-black after:rounded-sm after:block after:absolute after:top-[22px] after:left-0">
+                after:w-[30px] after:h-[5px] after:bg-black after:rounded-sm after:block after:absolute after:top-[22px] after:left-0">
             </div>
         </button>
         <ul v-if="menuView" class="absolute top-12 bg-white px-10 pl-14 py-5 -left-10 flex flex-col gap-5 rounded-br-lg">
