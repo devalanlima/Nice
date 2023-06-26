7de99e208edb7461ea96890b988de1b32a6ae370
Alan Lima <dev.alanlima@gmail.com>
2023-06-26 11:34:29 -0300

    add / SecaoBeneficios component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <script setup>
 import NavBar from './components/NavBar.vue';
 import BannerPrincipal from './components/BannerPrincipal.vue'
+import SecaoBeneficios from './components/SecaoBeneficios.vue';
 </script>
 
 <template>
   <header class="mb-6 px-5 pt-10">
     <NavBar/>
   </header>
-  <main>
+  <main class="flex flex-col gap-4">
     <BannerPrincipal/>
+    <SecaoBeneficios/>
   </main>
 </template>
