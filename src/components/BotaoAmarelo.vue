eb97db2c8d8a0e4f75ae846a58ac498df7c714f2
Alan Lima <dev.alanlima@gmail.com>
2023-06-27 16:57:53 -0300

    add / type prop

--- src/components/BotaoAmarelo.vue
+++ src/components/BotaoAmarelo.vue
@@ -1,11 +1,12 @@
 <template>
-    <button class="bg-[0%] bg-[length:205%_100%] bg-gradient-to-r from-amarelo from-50% to-transparent to-0% hover:transition-all transition-all hover:text-amarelo box-border px-12 py-3 self-baseline rounded-lg font-semibold text-roxo_escuro hover:bg-[99%] border-[1px] border-amarelo ">
+    <button class="bg-[0%] bg-[length:205%_100%] bg-gradient-to-r from-amarelo from-50% to-transparent to-0% hover:transition-all transition-all hover:text-amarelo box-border px-12 py-3 self-baseline rounded-lg font-semibold text-roxo_escuro hover:bg-[99%] border-[1px] border-amarelo" :type="props.type">
         {{props.text}}
     </button>
 </template>
 
 <script setup>
 const props = defineProps({
-    text:{Type:String, default:'Botão'}
+    text:{Type:String, default:'Botão'},
+    type:{Type:String, default:'button'}
 })
 </script>
--- src/components/BotaoTransparente.vue
+++ src/components/BotaoTransparente.vue
@@ -1,13 +1,14 @@
 <template>
     <button class="px-10 py-3 border-[1px] border-branco self-baseline rounded-lg font-semibold text-branco
     bg-gradient-to-r from-transparent from-50% to-branco to-50% bg-[length:205%_100%] hover:bg-[99%] transition-all hover:transition-all hover:text-roxo_escuro z-10 hover:border-transparent
-    ">
+    " :type="props.type">
         {{ props.text }}
     </button>
 </template>
 
 <script setup>
 const props = defineProps({
-    text:{Type:String, default:'Botão'}
+    text:{Type:String, default:'Botão'},
+    type:{Type:String, default:'button'}
 })
 </script>
