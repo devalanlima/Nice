5090881e8b764e0f735c5cacf4ed03a855d5ba3d
Alan Lima <dev.alanlima@gmail.com>
2023-06-28 13:39:15 -0300

    refactor / change validations for computed property and block button if invalid

--- src/components/SecaoContato.vue
+++ src/components/SecaoContato.vue
@@ -3,7 +3,7 @@
         <div class="flex flex-col gap-4 mx-auto px-16 pt-9">
             <img class="self-center" src="/images/Chat.png" alt="Imagem de um balão de fala">
             <h3 class="text-2xl text-center font-semibold text-roxo_escuro">Ainda tem dúvidas? Fale conosco!</h3>
-            <span class="w-[97px] h-[3px] bg-amarelo mx-auto"></span>
+            <span class="w-[97px] h-[3px] rounded-full bg-amarelo mx-auto"></span>
         </div>
         <div class="p-9 leading-normal text-cinza_escuro">
             <p>
@@ -21,71 +21,78 @@
             </a>
             <form action="fale_conosco" method="get" autocomplete="on" class="pt-12 flex flex-col gap-4">
                 <div class="flex flex-col gap-[5px]">
-                    <label for="nameContato" :class="['text-sm text-roxo font-medium',{'text-red-400': !nomeIsValid}]">Nome<span v-if="!nomeIsValid" class="text-red-400"> inválido</span></label>
+                    <label for="nameContato" :class="['text-sm text-roxo font-medium',{'text-red-500': !nomeIsValid}]">Nome<span v-if="!nomeIsValid" class="text-red-500"> inválido</span></label>
                     <input aria-label="Nome"
-                    :class="['w-full h-[40px] outline-[1px] outline-roxo_claro rounded-lg pl-4 py-2 text-cinza_escuro outline focus:outline-roxo_escuro focus:outline-2', {'focus:outline-red-400': !nomeIsValid},{'outline-red-400': !nomeIsValid}]"
+                    :class="['w-full h-[40px] outline-[1px] outline-roxo_claro rounded-lg pl-4 py-2 text-cinza_escuro outline focus:outline-roxo_escuro focus:outline-2', {'focus:outline-red-500 outline-red-500': !nomeIsValid}]"
                         type="text" id="nameContato" placeholder="Nome Sobrenome" autocomplete="name" required
                         v-model.trim="nome" @input="validateNome">
                 </div>
                 
                 <div class="flex flex-col gap-[5px]">
-                    <label for="emailContato" :class="['text-sm text-roxo font-medium', {'text-red-400': !emailIsValid}]">Email<span v-if="!emailIsValid" class="text-red-400"> inválido</span></label>
+                    <label for="emailContato" :class="['text-sm text-roxo font-medium', {'text-red-500': !emailIsValid}]">Email<span v-if="!emailIsValid" class="text-red-500"> inválido</span></label>
                     <input aria-label="Email"
-                        :class="['w-full h-[40px] outline-[1px] outline-roxo_claro rounded-lg pl-4 py-2 text-cinza_escuro outline focus:outline-roxo_escuro focus:outline-2', {'focus:outline-red-400': !emailIsValid},{'outline-red-400': !emailIsValid}]"
+                        :class="['w-full h-[40px] outline-[1px] outline-roxo_claro rounded-lg pl-4 py-2 text-cinza_escuro outline focus:outline-roxo_escuro focus:outline-2', {'focus:outline-red-500 outline-red-500': !emailIsValid}]"
                         type="email" id="emailContato" placeholder="nome@email.com.br" autocomplete="email" required
-                        v-model="email" @input="validateEmail">
+                        v-model="email" @input="validateEmail"
+                        pattern="^[^\s@]+@[^\s@]+\.[^\s@]{2,}$"
+                        >
                 </div>
                 
                 <div class="flex flex-col gap-[5px]">
                     <label for="telefoneContato" 
-                    :class="['text-sm text-roxo font-medium', {'text-red-400': !telefoneIsValid}]"
-                    >Telefone<span v-if="!telefoneIsValid" class="text-red-400"> inválido</span></label>
+                    :class="['text-sm text-roxo font-medium', {'text-red-500': !telefoneIsValid}]"
+                    >Telefone<span v-if="!telefoneIsValid" class="text-red-500"> inválido</span></label>
                     <input aria-label="Telefone"
-                    :class="['w-full h-[40px] outline-[1px] outline-roxo_claro rounded-lg pl-4 py-2 text-cinza_escuro outline focus:outline-roxo_escuro focus:outline-2', {'focus:outline-red-400': !telefoneIsValid},{'outline-red-400': !telefoneIsValid}]"
+                    :class="['w-full h-[40px] outline-[1px] outline-roxo_claro rounded-lg pl-4 py-2 text-cinza_escuro outline focus:outline-roxo_escuro focus:outline-2', {'focus:outline-red-500 outline-red-500': !telefoneIsValid}]"
                         type="tel" id="telefoneContato" placeholder="(00)0000-0000" maxlength="14" autocomplete="tel"
                         required v-model="telefone" @input="validateTelefone" 
+                        pattern="^\(\d{2}\)\d{4,5}-\d{4}$"
                         >
                 </div>
-                <BotaoAmarelo class="mt-8 mx-auto mr-0" text="Enviar!" type="submit"/>
+                <BotaoAmarelo class="mt-8 mx-auto mr-0" text="Enviar!" type="submit" :disabled="!nomeIsValid || !emailIsValid || !telefoneIsValid ||nome.length === 0 || email.length === 0 || telefone.length === 0 "/>
             </form>
         </div>
     </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import BotaoAmarelo from './BotaoAmarelo.vue';
 const nome = ref('')
-const nomeIsValid = ref(true)
 const email = ref('')
-const emailIsValid = ref(true)
 const telefone = ref('')
-const telefoneIsValid = ref(true)
 
 const validateNome = () => {
     const nomeRegex = /^[\p{L}\p{M}\s']{2,}(?:[\s'-][\p{L}\p{M}']+){1,}$/u
-    if (nomeRegex.test(nome.value)) {
-        nomeIsValid.value = true
+    if (nomeRegex.test(nome.value) || nome.value.length === 0) {
+        return true
     } else {
-        nomeIsValid.value = false
+        return false
     }
-    
 }
+
+const nomeIsValid = computed(()=>{
+    return validateNome()
+})
 
 const validateEmail = () => {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/
-    if (emailRegex.test(email.value)) {
-        emailIsValid.value = true
+    if (emailRegex.test(email.value) || email.value.length === 0) {
+        return true
     } else {
-        emailIsValid.value = false
+        return false
     }
 }
+
+const emailIsValid = computed(()=>{
+    return validateEmail()
+})
 
 const newLength = ref(0)
 const oldLength = ref(0)
 const validateTelefone = () => {
     const telefoneRegex = /^\(\d{2}\)\d{4,5}-\d{4}$/
-
+    telefone.value = telefone.value.replace(/[^()\d-]+/,'')
     newLength.value = telefone.value.length
     if (newLength.value > oldLength.value){
         if(newLength.value === 1){
@@ -104,11 +111,14 @@ const validateTelefone = () => {
     }
     oldLength.value = telefone.value.length
 
-    if (telefoneRegex.test(telefone.value)) {
-        telefoneIsValid.value = true
+    if (telefoneRegex.test(telefone.value) || telefone.value.length === 0) {
+        return true
     } else {
-        telefoneIsValid.value = false
+        return false
     }
 }
 
+const telefoneIsValid = computed(()=>{
+    return validateTelefone()
+})
 </script>
