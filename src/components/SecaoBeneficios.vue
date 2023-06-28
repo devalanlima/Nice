d3a41fc7250fcfd8d55c6ae276ac05e8a44867a9
Alan Lima <dev.alanlima@gmail.com>
2023-06-28 13:35:10 -0300

    stylize / add yellow line

--- src/components/SecaoBeneficios.vue
+++ src/components/SecaoBeneficios.vue
@@ -1,9 +1,12 @@
 <template>
     <div class="flex flex-col">
-        <h3 class="text-center text-2xl text-roxo_escuro font-semibold pl-14 pr-16 tracking-tighter leading-normal mb-6">Um subtítulo para quem
-            rolou até aqui!</h3>
-        <p class="px-4 text-center tracking-tight text-cinza_escuro leading-normal">Aproveite esta seção para destacar os benefícios do seu
-            produto ou serviço. Palavras-chave ajudam no SEO.</p>
+        <div class="flex flex-col gap-3">
+            <h3 class="text-center text-2xl text-roxo_escuro font-semibold pl-14 pr-16 tracking-tighter leading-normal">Um subtítulo para quem
+                rolou até aqui!</h3>
+                <span class="w-[97px] h-[3px] rounded-full bg-amarelo mx-auto"></span>
+            <p class="px-4 text-center tracking-tight text-cinza_escuro leading-normal">Aproveite esta seção para destacar os benefícios do seu
+                produto ou serviço. Palavras-chave ajudam no SEO.</p>
+        </div>
         <div class="flex pl-8 gap-6 w-full overflow-x-auto mt-3 pr-8 pb-14">
             <BeneficioCard 
                 title="Rápido"
