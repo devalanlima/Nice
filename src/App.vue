01fd9c8a10fe470262f53338a628b8607ff53414
Alan Lima <dev.alanlima@gmail.com>
2023-06-29 16:12:26 -0300

    create / back to top logic

--- src/App.vue
+++ src/App.vue
@@ -5,19 +5,48 @@ import SecaoBeneficios from './components/SecaoBeneficios.vue';
 import SecaoQuemSomos from './components/SecaoQuemSomos.vue';
 import SecaoContato from './components/SecaoContato.vue';
 import SecaoFinal from './components/SecaoFinal.vue';
+import BackToTop from './components/BackToTop.vue';
+
+import { onMounted, ref } from 'vue'
+const myHeader = ref(null)
+const isShow = ref(false)
+const verifica = () => {
+  let rect = false
+  try {
+    rect = myHeader.value.getBoundingClientRect()
+  } catch (error) {
+    rect = false
+  }
+  if(rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)){
+      isShow.value = false
+    } else {
+      isShow.value = true
+    }
+}
+
+onMounted(() => {
+  window.addEventListener('scroll', function () {
+    verifica()
+  });
+})
 </script>
 
 <template>
-  <header class="mb-6 px-5 pt-10 xl:px-28">
-    <NavBar/>
+  <header id="header" class="mb-6 px-5 pt-10 xl:px-28" ref="myHeader">
+    <NavBar />
   </header>
   <main class="flex flex-col">
-    <BannerPrincipal/>
-    <SecaoBeneficios/>
-    <SecaoQuemSomos   class="pb-20"/>
-    <SecaoContato/>
+    <BannerPrincipal />
+    <SecaoBeneficios id="secao-beneficios" />
+    <SecaoQuemSomos id="secao-quemsomos" class="pb-20" />
+    <SecaoContato id="secao-faleconosco" />
+    <BackToTop v-if="isShow" class="fixed right-10 bottom-10"/>
+
   </main>
   <footer>
-    <SecaoFinal/>
+    <SecaoFinal />
   </footer>
 </template>
