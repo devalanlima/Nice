8e081a8e9c16714de74cb4c6a4e469e55a74d9f8
Alan Lima <dev.alanlima@gmail.com>
2023-06-29 13:09:54 -0300

    fix / visual style  bug

--- src/components/SecaoBeneficios.vue
+++ src/components/SecaoBeneficios.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex flex-col w-full max-w-[1000px] mx-auto">
+    <div class="flex flex-col w-full max-w-[1100px] mx-autogi">
         <div class="flex flex-col gap-3">
             <h3 class="text-center text-2xl text-roxo_escuro font-semibold pl-14 pr-16 tracking-tighter leading-normal">Um subtítulo para quem
                 rolou até aqui!</h3>
@@ -7,7 +7,7 @@
             <p class="px-4 text-center tracking-tight text-cinza_escuro leading-normal max-w-[650px] mx-auto">Aproveite esta seção para destacar os benefícios do seu
                 produto ou serviço. Palavras-chave ajudam no SEO.</p>
         </div>
-        <div class="flex pl-8 gap-6 w-full overflow-x-auto mt-3 pr-8 pb-14">
+        <div class="flex pl-8 gap-6 w-full justify-center overflow-x-auto mt-3 pr-8 pb-14">
             <BeneficioCard 
                 title="Rápido"
                 description="Aproveite esta seção para destacar os benefícios do seu produto ou serviço.">
