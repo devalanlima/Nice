3a62eb93d77a4a4d36f58333168948b13be2c244
Alan Lima <dev.alanlima@gmail.com>
2023-06-29 16:11:07 -0300

    create / BackToTop.vue component

---
+++ src/components/BackToTop.vue
@@ -0,0 +1,9 @@
+<template>
+    <a href="#header"
+        class="opacity-50 hover:opacity-100 hover:scale-105 transition-all hover:transition-all">
+        <img class="w-10" src="/icons/arrowup.svg" alt="seta para cima">
+        <span class="sr-only">
+            Voltar ao topo
+        </span>
+    </a>
+</template>
